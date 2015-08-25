6d9d9e2cbb78ef92b7bcf114eb859d600772ec35
Isaac Ezer <isaac.ezer@gmail.com>
2015-08-25 12:26:37 -0400

    add deprecations from running app, not in test mode

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,21 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchMessage: "Using the same function as getter and setter is deprecated." }
+
+    // Deprecations from Running Test Suite
+    { handler: "silence", matchMessage: "Using the same function as getter and setter is deprecated." },
+
+    //Deprecations from running real app only
+    { handler: "silence", matchMessage: "You modified ShouldDisplay(graph.hasData) twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([d]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([height]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([graphHeight]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified view.transform twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([leftX]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([rightWidth]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([rightX]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified each(ticks) twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([width]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" },
+    { handler: "silence", matchMessage: "You modified concat([graphWidth]; separator=\"\") twice in a single render. This was unreliable in Ember 1.x and will be removed in Ember 2.0" }
   ]
 };
